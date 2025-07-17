6e06dcd13fcc4b9d69aebd93b36fe8d74347f13e
sumithrasivakumar <sumithrasiva33@gmail.com>
2025-07-17 22:24:52 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,37 +1,16 @@
 package practice;
 import java.util.Scanner;
+class Start{
+    public void circle(float f){
+        System.out.println(3.14*f*f);
+    }
+}
 
-public class Main{
+class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Input sentence
-        System.out.println("Enter a sentence:");
-        String sentence = scanner.nextLine().toLowerCase();
-
-        // Array to mark presence of each alphabet letter
-        boolean[] alphabetPresent = new boolean[26];
-
-        // Marking letters present in the sentence
-        for (int i = 0; i < sentence.length(); i++) {
-            char ch = sentence.charAt(i);
-            if (ch >= 'a' && ch <= 'z') {
-                alphabetPresent[ch - 'a'] = true;
-            }
-        }
-
-        // Check and print missing letters
-        boolean allPresent = true;
-        System.out.print("Missing alphabets: ");
-        for (int i = 0; i < 26; i++) {
-            if (!alphabetPresent[i]) {
-                System.out.print((char) (i + 'a') + " ");
-                allPresent = false;
-            }
-        }
-
-        if (allPresent) {
-            System.out.println("None (It's a pangram)");
-        }
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        Start obj1=new Start();
+        obj1.circle(a);
     }
 }
